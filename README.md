9fe14fd41e85706e7e6cc89b7d704bca2e8c52db
웹스토리보이 <webstoryboy@naver.com>
2024-11-04 19:45:24 +0900

    2024.11.04

---
+++ README.md
@@ -0,0 +1,23 @@
+# 파이썬을 이용한 뮤직 데이터 수집하기
+
+파이썬을 이용하여 뮤직 순위정보를 수집하여 사이트를 만들겠습니다.
+
+## 애플 뮤직 챠트 수집하기
+
+애플 뮤직 사이트 https://music.apple.com/kr/new
+
+### Selenium
+
+Selenium은 웹 브라우저를 자동화하는 도구로, 웹 애플리케이션을 테스트하거나 데이터를 수집하는 데 많이 사용됩니다.
+
+```bash
+pip install selenium
+```
+
+### BeautifulSoup
+
+BeautifulSoup은 HTML이나 XML 파일을 파싱하여 원하는 정보를 쉽게 추출할 수 있게 해주는 도구입니다. BeautifulSoup은 웹 크롤링과 스크래핑 작업에서 많이 사용되며, HTML 문서 구조를 파싱하여 특정 태그, 속성, 클래스 등을 통해 데이터를 추출할 수 있습니다.
+
+```bash
+pip install beautifulsoup4
+```
---
+++ appleChart.py
@@ -0,0 +1,77 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from bs4 import BeautifulSoup
+from datetime import datetime
+import os
+import json
+
+# 현재 날짜 가져오기
+current_date = datetime.now().strftime("%Y-%m-%d")
+folder_path = "apple"
+filename = f"{folder_path}/apple100_{current_date}.json"
+
+# 폴더가 없으면 생성
+os.makedirs(folder_path, exist_ok=True)
+
+# 웹드라이버 설정 및 페이지 로드
+options = ChromeOptions()
+options.add_argument("--headless")
+browser = webdriver.Chrome(options=options)
+browser.get("https://music.apple.com/kr/playlist/%EC%98%A4%EB%8A%98%EC%9D%98-top-100-%EB%8C%80%ED%95%9C%EB%AF%BC%EA%B5%AD/pl.d3d10c32fbc540b38e266367dc8cb00c")
+
+# 페이지가 완전히 로드될 때까지 대기
+try:
+    WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "songs-list"))
+    )
+    print("페이지가 완전히 로드되었습니다.")
+except TimeoutException:
+    print("요소를 찾는 데 실패했습니다. 페이지가 완전히 로드되지 않았습니다.")
+    browser.quit()
+    exit()
+
+# 업데이트된 페이지 소스를 변수에 저장
+html_source_updated = browser.page_source
+soup = BeautifulSoup(html_source_updated, 'html.parser')
+
+# 노래 정보를 추출
+song_data = []
+songs_list = soup.find_all('div', class_='songs-list-row', role='row')
+for song in songs_list:
+    ranking = song.find('div', class_='songs-list-row__rank')
+    title = song.find('div', class_='songs-list-row__song-name')
+    artist_tag = song.find_all('a', {'data-testid': 'click-action'})
+    
+    # 요소가 존재하는지 확인 후 데이터 추출
+    ranking_text = ranking.text.strip() if ranking else "No ranking"
+    title_text = title.text.strip() if title else "No title"
+    artist_text = artist_tag[0].text.strip() if artist_tag else "No artist"
+    album_text = artist_tag[1].text.strip() if len(artist_tag) > 1 else "No album"
+    
+    # 이미지 URL 추출
+    img_tag = song.find('picture').find('source', type="image/webp") if song.find('picture') else None
+    if img_tag:
+        image_sources = img_tag.get('srcset', "")
+        image_url = next((src.split(' ')[0] for src in image_sources.split(',') if '80x80bb.webp' in src), "No image available")
+    else:
+        image_url = "No image available"
+    
+    song_data.append({
+        'ranking': ranking_text,
+        'title': title_text,
+        'artist': artist_text,
+        'album': album_text,
+        'image_url': image_url
+    })
+
+# 추출된 데이터를 JSON 파일로 저장
+with open(filename, 'w', encoding='utf-8') as f:
+    json.dump(song_data, f, ensure_ascii=False, indent=4)
+    print(f"데이터가 '{filename}' 파일에 저장되었습니다.")
+  
+# 브라우저 종료
+browser.quit()
